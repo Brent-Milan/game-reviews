447c6fbe7aedfed83e2be07f5b532315767a4711
Brent <Bmilan24@gmail.com>
2017-06-29 14:05:58 -0400

    GameRepository implemented (extends Crud)

--- src/main/java/production/GameController.java
+++ src/main/java/production/GameController.java
@@ -5,4 +5,12 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class GameController {
 	
+	@Resource 
+	private GameRepository gameRepo;
+	
+	@RequestMapping("/reviews")
+	public String fetchGames(Model model) {
+		model.addAtrribute("games", gameRepo.findAll());
+	}
+		return "reviews";
 }
